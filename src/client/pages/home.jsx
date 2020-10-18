8c8e1f86e02edd1ac6081c13dc8b6a6e43d59f06
unknown <sergiop.pias@gmail.com>
2020-10-18 23:25:49 +0200

    create homepage

---
+++ src/client/pages/home.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/header/Header";
+import Home from "../components/home/mainpage";
+
+const HomePage = () => {
+  return <div className="container">
+    <Header/>
+    <Home/>
+  </div>;
+};
+
+export default HomePage;
