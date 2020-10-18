4105baa884397e3db16ccf7fcd7897d00b4c6933
unknown <sergiop.pias@gmail.com>
2020-10-18 23:25:32 +0200

    added header to router

--- src/client/components/AppRouter.jsx
+++ src/client/components/AppRouter.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./home/mainpage";
 import NotFound from "./notFound/NotFound";
+import Nav from "./header/Header"
+
 const Routes = () => {
   return (
     <Router>
+      <Nav>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path='*' component={NotFound}/>
       </Switch>
+      </Nav>
     </Router>
   );
 };
