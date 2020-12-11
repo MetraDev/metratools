10097e065991564515c5c74c3f9629b087c75d25
unknown <sergiop.pias@gmail.com>
2020-12-12 00:41:18 +0100

    added new component

--- src/client/components/home/mainpage.jsx
+++ src/client/components/home/mainpage.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
-const HeaderPlan = () => {
-  return <div className="container bg-dark">Hola web</div>;
+import Grid from "@material-ui/core/Grid";
+import PresentationCard from './presentation/PresentationCard'
+const Home = () => {
+  return (
+    <Grid container spacing={1}>
+      <Grid
+        align="right"
+        xs={2}
+        style={{ "margin-top": "12px"}}
+        className="container zindex-fixed"
+      >
+        <img
+          src="https://pbs.twimg.com/profile_images/1325247524815429633/dh3G5c-u_400x400.jpg"
+          class="img-thumbnail"
+          alt="Responsive image"
+        />
+      </Grid>
+      <Grid xs={10} style={{ "margin-top": "13px" }}>
+        <PresentationCard />
+      </Grid>
+    </Grid>
+  );
 };
 
-export default HeaderPlan;
+export default Home;
