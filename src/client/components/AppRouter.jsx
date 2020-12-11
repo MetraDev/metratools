97415140169b47d8bdb79230bf0a5d57e81f6ddd
unknown <sergiop.pias@gmail.com>
2020-12-12 00:43:32 +0100

    fixed nav

--- src/client/components/AppRouter.jsx
+++ src/client/components/AppRouter.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./home/mainpage";
 import NotFound from "./notFound/NotFound";
-import Nav from "./header/Header"
+import Nav from "./header/Header";
+
 
 const Routes = () => {
   return (
     <Router>
-      <Nav>
+      <Nav />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path='*' component={NotFound}/>
+        <Route path="*" component={NotFound} />
       </Switch>
-      </Nav>
     </Router>
   );
 };
